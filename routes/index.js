ad6cbb3dec149b05fbc795651a4524422ff1ae17
martin <martin@xsolon.net>
2018-03-03 03:19:27 -0500

    vs node project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'spExplorer' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "spExplorer", version: "0.0.2" });
 });
 
 module.exports = router;
