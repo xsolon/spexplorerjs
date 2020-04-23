9f57ae58d889019e7302fd981da99f7fd79cb201
martin <martin@xsolon.net>
2020-04-23 11:48:20 -0400

    // 2020-04-21 - tsClassBuilder: support for null parameter in setter template

--- nodeapi/spexplorerjs.node/src/api/meta.api.ts
+++ nodeapi/spexplorerjs.node/src/api/meta.api.ts
@@ -1,105 +1,106 @@
-﻿// 2020-03-27 - FieldMeta: added flags(variable to store attributes, not used internally), ListMeta: getConfig (variable to store a function, not used internally)
+﻿// 2020-04-21 - tsClassBuilder: support for null parameter in setter template
+// 2020-03-27 - FieldMeta: added flags(variable to store attributes, not used internally), ListMeta: getConfig (variable to store a function, not used internally)
 // 2020-03-23 - FieldMeta: added multiValue
 import { ListApi } from "./list.api";
 
 export class ListMeta {
-    public fields: FieldMeta[];
-    public ctypes: CTypeMeta[];
-    public listTemplate: number;
-    public title: string;
-    public defaultItems: Array<{ [key: string]: any }> | itemsFunction;
-    public afterDefaultItemsAdded?: postItemsAddedFunction;
-    public listUpdates?: listUpdatesFunction;
-    public afterListCreated?: listUpdatesFunction;
-    public permissions?: GroupMeta[];
-    public getConfig?: () => any;
+  public fields: FieldMeta[];
+  public ctypes: CTypeMeta[];
+  public listTemplate: number;
+  public title: string;
+  public defaultItems: Array<{ [key: string]: any }> | itemsFunction;
+  public afterDefaultItemsAdded?: postItemsAddedFunction;
+  public listUpdates?: listUpdatesFunction;
+  public afterListCreated?: listUpdatesFunction;
+  public permissions?: GroupMeta[];
+  public getConfig?: () => any;
 
-    constructor(title: string) {
-        this.title = title;
-        this.fields = [];
-        this.defaultItems = [];
-    }
+  constructor(title: string) {
+    this.title = title;
+    this.fields = [];
+    this.defaultItems = [];
+  }
 
-    public static version: '0.1.4';
+  public static version: '0.1.4';
 }
 
 export class GroupMeta {
-    name: string;
-    permissions: [string];
-    desc?: string = '';
+  name: string;
+  permissions: [string];
+  desc?: string = '';
 }
 
 export class FieldMeta {
-    public markup: markupFunction | string;
-    public name: string;
-    public type?: SP.FieldType;
-    public flags?: number = 0;
-    public multiValue?: boolean = false;
-    public legacyName?: string | null;
-    public title?: string | null;
-    public post?: postFunction;
-    public inDefaultView?: boolean = false;
-    public addOptions?: SP.AddFieldOptions = SP.AddFieldOptions.addFieldInternalNameHint | SP.AddFieldOptions.addToAllContentTypes
+  public markup: markupFunction | string;
+  public name: string;
+  public type?: SP.FieldType;
+  public flags?: number = 0;
+  public multiValue?: boolean = false;
+  public legacyName?: string | null;
+  public title?: string | null;
+  public post?: postFunction;
+  public inDefaultView?: boolean = false;
+  public addOptions?: SP.AddFieldOptions = SP.AddFieldOptions.addFieldInternalNameHint | SP.AddFieldOptions.addToAllContentTypes
 }
 
 export class FieldLinkMeta {
-    public name: string;
-    public hidden?: boolean = null;
+  public name: string;
+  public hidden?: boolean = null;
 }
 
 export class CTypeMeta {
-    public name: string;
-    public parentCtypeId: string;
-    public fields: FieldLinkMeta[];
-    public group?: string;
-    public description?: string;
-    public jsLink?: string;
+  public name: string;
+  public parentCtypeId: string;
+  public fields: FieldLinkMeta[];
+  public group?: string;
+  public description?: string;
+  public jsLink?: string;
 }
 
 export var classBuilder = function (list: ListMeta): string {
 
-    var fieldStr = "";
-    list.fields.forEach(function (field) {
-        fieldStr += `public static ${field.name} : string = "${field.name}";\n`;
-    });
-    var template = `export class ${list.title}Def {
+  var fieldStr = "";
+  list.fields.forEach(function (field) {
+    fieldStr += `public static ${field.name} : string = "${field.name}";\n`;
+  });
+  var template = `export class ${list.title}Def {
 	${fieldStr}
 }`
-    return template;
+  return template;
 };
 export var tsClassBuilder = function (list: ListMeta): string {
 
-    var ctypes: CTypeMeta[] = JSON.parse(JSON.stringify(list.ctypes || [])); // copy
+  var ctypes: CTypeMeta[] = JSON.parse(JSON.stringify(list.ctypes || [])); // copy
 
-    if (ctypes.length == 0) {
-        //@ts-ignore
-        ctypes.push({
-            name: '',
-            fields: list.fields,
-        });
+  if (ctypes.length == 0) {
+    //@ts-ignore
+    ctypes.push({
+      name: '',
+      fields: list.fields,
+    });
+  }
+  var fields: { [key: string]: string } = {};
+  list.fields.forEach((f) => {
+    var sType = "string"
+    if (f.type == SP.FieldType.user) {
+      sType = 'SP.FieldUserValue';
     }
-    var fields: { [key: string]: string } = {};
-    list.fields.forEach((f) => {
-        var sType = "string"
-        if (f.type == SP.FieldType.user) {
-            sType = 'SP.FieldUserValue';
-        }
-        else if (f.type == SP.FieldType.lookup) {
-            sType = 'SP.FieldLookupValue';
-        }
-        else if (f.type == SP.FieldType.boolean) {
-            sType = 'boolean';
-        }
-        else if (f.type == SP.FieldType.dateTime) {
-            sType = 'Date';
-        }
+    else if (f.type == SP.FieldType.lookup) {
+      sType = 'SP.FieldLookupValue';
+    }
+    else if (f.type == SP.FieldType.boolean) {
+      sType = 'boolean';
+    }
+    else if (f.type == SP.FieldType.dateTime) {
+      sType = 'Date';
+    }
 
-        if (f.multiValue) {
-            sType += '[]';
-        }
-        var tmp = `${f.name}(val?: ${sType}): ${sType} {
+    if (f.multiValue) {
+      sType += '[]';
+    }
+    var tmp = `${f.name}(val?: ${sType}): ${sType} {
     var me = this;
-    if (val) {
+    if (arguments.length) {
       me.li.set_item('${f.name}', val);
     }
     var res: ${sType} = me.li.get_item('${f.name}');
@@ -107,20 +108,20 @@ export var tsClassBuilder = function (list: ListMeta): string {
   }
 
 `;
-        //fields.push( );
-        fields[f.name] = tmp;
+    //fields.push( );
+    fields[f.name] = tmp;
+  });
+
+  var res = '/// <reference types="sharepoint" />';
+  ctypes.forEach((c) => {
+
+    var ctypeFields = [];
+    c.fields.forEach((f) => {
+      ctypeFields.push(fields[f.name]);
     });
 
-    var res = '/// <reference types="sharepoint" />';
-    ctypes.forEach((c) => {
-
-        var ctypeFields = [];
-        c.fields.forEach((f) => {
-            ctypeFields.push(fields[f.name]);
-        });
-
-        var className = `${list.title.replace(/ /g, '')}${c.name.replace(/ /g, '')}Type`;
-        res += `
+    var className = `${list.title.replace(/ /g, '')}${c.name.replace(/ /g, '')}Type`;
+    res += `
 export class ${className} {
   li: SP.ListItem;
   constructor(li?: SP.ListItem) {
@@ -134,7 +135,7 @@ export class ${className} {
   }
   id(val?: number): number{
     var me = this;
-    if (val) {
+    if (arguments.length) {
       me.li.set_item('ID', val);
     }
     var res: number = me.li.get_item('ID');
@@ -142,7 +143,7 @@ export class ${className} {
   }
   title (val?: string): string {
     var me = this;
-    if (val) {
+    if (arguments.length) {
       me.li.set_item('Title', val);
     }
     var res: string = me.li.get_item('Title');
@@ -185,8 +186,8 @@ export class ${className} {
 }
 `;
 
-    });
-    return res;
+  });
+  return res;
 };
 
 
